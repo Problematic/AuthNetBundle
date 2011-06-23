ddd1b55fd094dcfb7ece44ff9ea509b7d849704a
Derek Stobbe <djstobbe@gmail.com>
2011-06-23 17:17:34 -0600

    added skeleton controllers, routing, and views

---
+++ Controller/ArbController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Problematic\AuthNetBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ArbController extends Controller
+{
+
+    public function createSubscriptionAction()
+    {
+        
+    }
+    
+    public function updateSubscriptionAction()
+    {
+        
+    }
+    
+    public function getSubscriptionStatusAction()
+    {
+        
+    }
+    
+    public function cancelSubscriptionAction()
+    {
+        
+    }
+    
+}
