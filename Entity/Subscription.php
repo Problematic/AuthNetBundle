ce6ce4195455803b4a18c6d56a058841bf0d9d57
Derek Stobbe <djstobbe@gmail.com>
2011-06-24 13:15:43 -0600

    added Subscription entity

---
+++ Entity/Subscription.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Problematic\AuthNetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class Subscription
+{
+
+    /**
+     * @ORM\Id @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected $id;
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $created_at;
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $updated_at;
+    /**
+     * @ORM\Column(type="integer", length="13")
+     */
+    protected $subscription_id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $ref_id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function setCreatedAt(\DateTime $created_at)
+    {
+        $this->created_at = $created_at;
+    }
+    
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+    
+    public function setUpdatedAt(\DateTime $updated_at)
+    {
+        $this->updated_at = $updated_at;
+    }
+    
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    public function setSubscriptionId($subscription_id)
+    {
+        $this->subscription_id = $subscription_id;
+    }
+
+    public function getSubscriptionId()
+    {
+        return $this->subscription_id;
+    }
+
+    public function setRefId($ref_id)
+    {
+        $this->ref_id = $ref_id;
+    }
+
+    public function getRefId()
+    {
+        return $this->ref_id;
+    }
+    
+    /**
+     * @ORM\PrePersist
+     */
+    public function onPrePersist()
+    {
+        $this->setCreatedAt(new \DateTime());
+        $this->setUpdatedAt(new \DateTime());
+    }
+    
+    /**
+     * @ORM\PreUpdate
+     */
+    public function onPreUpdate()
+    {
+        $this->setUpdatedAt(new \DateTime());
+    }
+
+}
