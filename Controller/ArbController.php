25dd59d3a1c7dea132b0a8f4ea143dc14b5df3c2
Derek Stobbe <djstobbe@gmail.com>
2011-06-24 11:54:38 -0600

    basic subscription create functionality

--- Controller/ArbController.php
+++ Controller/ArbController.php
@@ -3,13 +3,31 @@
 namespace Problematic\AuthNetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Problematic\AuthNetBundle\AuthorizeNet\Shared\DataType\AuthorizeNet_Subscription;
+use Problematic\AuthNetBundle\AuthorizeNet\API\ARB\AuthorizeNetARB;
 
 class ArbController extends Controller
 {
 
     public function createSubscriptionAction()
     {
+        $request = $this->getRequest();
+        $subscription = new AuthorizeNet_Subscription();
+        $form = $this->createForm($type, $subscription, $options);
         
+        if ('POST' == $request->getMethod()) {
+            $form->bindRequest($request);
+            
+            $arbRequest = new AuthorizeNetARB();
+            $response = $arbRequest->createSubscription($subscription);
+            if ($response->isOk()) {
+                $subscriptionId = $response->getSubscriptionId();
+            }
+            
+            // todo: redirect to success page
+        }
+        
+        return new \Symfony\Component\HttpFoundation\Response();
     }
     
     public function updateSubscriptionAction()
