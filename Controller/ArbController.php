f78b5fcc8fdc2c82919ed37b05f837acd7dc5c42
Derek Stobbe <djstobbe@gmail.com>
2011-06-24 13:57:40 -0600

    fleshed out createSubscription action, added minimal getSubscriptionStatus action

--- Controller/ArbController.php
+++ Controller/ArbController.php
@@ -3,14 +3,21 @@
 namespace Problematic\AuthNetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Problematic\AuthNetBundle\AuthorizeNet\Shared\DataType\AuthorizeNet_Subscription;
 use Problematic\AuthNetBundle\AuthorizeNet\API\ARB\AuthorizeNetARB;
+use Problematic\AuthNetBundle\Entity\Subscription;
 
 class ArbController extends Controller
 {
 
     public function createSubscriptionAction()
     {
+        if (!$this->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw new AccessDeniedException();
+        }
+        
+        $em = $this->getDoctrine()->getEntityManager();
         $request = $this->getRequest();
         $subscription = new AuthorizeNet_Subscription();
         $form = $this->createForm($type, $subscription, $options);
@@ -18,10 +25,19 @@ class ArbController extends Controller
         if ('POST' == $request->getMethod()) {
             $form->bindRequest($request);
             
-            $arbRequest = new AuthorizeNetARB();
-            $response = $arbRequest->createSubscription($subscription);
-            if ($response->isOk()) {
-                $subscriptionId = $response->getSubscriptionId();
+            if ($form->isValid()) {
+                $create_request = new AuthorizeNetARB();
+                $create_request->setRefId($this->get('security.context')->getToken()->getUser()->getId());
+                $response = $create_request->createSubscription($subscription);
+
+                if ($response->isOk()) {
+                    $subscription = new Subscription();
+                    $subscription->setSubscriptionId($response->getSubscriptionId());
+                    $subscription->setRefId($response->getRefID());
+
+                    $em->persist($subscription);
+                    $em->flush();
+                }
             }
             
             // todo: redirect to success page
@@ -35,9 +51,13 @@ class ArbController extends Controller
         
     }
     
-    public function getSubscriptionStatusAction()
+    public function getSubscriptionStatusAction(Subscription $subscription)
     {
+        $em = $this->getDoctrine()->getEntityManager();
         
+        $status_request = new AuthorizeNetARB();
+        $response = $status_request->getSubscriptionStatus($subscription->getSubscriptionId());
+        $status = $response->getSubscriptionStatus();
     }
     
     public function cancelSubscriptionAction()
